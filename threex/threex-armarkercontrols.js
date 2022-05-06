066717222d2c69b8f8028ff9e16eecd25b2a0770
lucas-lightshape <91892543+lucas-lightshape@users.noreply.github.com>
2022-05-06 12:20:54 +0200

    Update threex-armarkercontrols.js

--- threex/threex-armarkercontrols.js
+++ threex/threex-armarkercontrols.js
@@ -24,11 +24,11 @@ ARjs.MarkerControls = THREEx.ArMarkerControls = function(context, object3d, para
 		minConfidence: 0,
 		smooth: true,
 		  // number of matrices to smooth tracking over, more = smoother but slower follow
-		  smoothCount: 50000,
+		  smoothCount: 5000,
 		  // distance tolerance for smoothing, if smoothThreshold # of matrices are under tolerance, tracking will stay still
-		  smoothTolerance: 75,
+		  smoothTolerance: 50000,
 		  // threshold for smoothing, will keep still unless enough matrices are over tolerance
-		  smoothThreshold: 500000
+		  smoothThreshold: 50000
 	}
 
 	// sanity check
